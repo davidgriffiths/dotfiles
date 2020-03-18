be442db727ab55bd65daef7c715dcc1ff2346967
David Griffiths <d@vid.gr>
2020-03-18 13:12:53 +0000

    Fix fish_prompt vs pure_prompt conflict
    `fish_prompt` was conflicting with pure, causing pure not to init when
    starting a new session.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,22 +1,16 @@
 #!/usr/bin/env fish
 set fish_greeting
-
 set -gx EDITOR nvim
 set -gx LANG en_US.UTF-8
-
 set homebrew /usr/local/bin /usr/local/sbin
 set default_path /usr/bin /usr/sbin /bin /sbin
-
 set -gx PATH $homebrew $default_path
-
 set -x FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
 
-# rbenv
-status --is-interactive; and source (rbenv init -|psub)
+# set pure prompt symbol
+set -g pure_symbol_prompt ">"
 
 alias vim='nvim'
 
-function fish_prompt
-  printf '%s' (prompt_pwd)
-  printf ' > '
-end
+# rbenv
+status --is-interactive; and source (rbenv init -|psub)
